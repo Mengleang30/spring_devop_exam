a2ffd9787cd679f3c4718d4eef16781e23e7dffb
Mengleang30 <novmengleang14@gmail.com>
2025-05-07 14:50:01 +0700

    Push spring project into gitHub

---
+++ src/main/java/mengleang/devop_exam/DevopExamApplication.java
@@ -0,0 +1,13 @@
+package mengleang.devop_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mengleang/devop_exam/DevopExamApplicationTests.java
@@ -0,0 +1,13 @@
+package mengleang.devop_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
